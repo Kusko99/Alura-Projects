aab5e692d2b7f00e7cf5ca46277a3532f79d19e0
Kusko99 <fernandaofo120@gmail.com>
2023-06-23 12:58:31 -0300

    Projeto do AluraMid feito em curso da Alura com objeto de estudo para JavaScript

--- JavaScript/Alura-Mid/main.js
+++ JavaScript/Alura-Mid/main.js
@@ -1,9 +1,39 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seleteorAudio){
+    const elemento = document.querySelector(seleteorAudio);
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        // alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
-// function tocaSomClap(){
-//     document.querySelector('#som_tecla_clap').play();
-// }
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelectorAll('.tecla');
+
+
+for(let contador = 0 ; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`
+    // console.log(idAudio)
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === "Enter" || evento.code === "Space"){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+    // console.log(contador);
+}
