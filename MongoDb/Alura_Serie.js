dbd22f3d5ae63eedf072043b7d614ccd67777e8f
Kusko99 <fernandaofo120@gmail.com>
2023-08-29 21:40:43 -0300

    Study database in MongoDb called Alura_Serie

--- MongoDb/Alura_Serie.js
+++ MongoDb/Alura_Serie.js
@@ -28,6 +28,14 @@ db.serie.insertOne({
     "Classificação" : "16+"
 })
 
+db.series.insertOne({
+    "Série" : "A nova onda do Kusko",
+    "Ano de Lançamento" : 2000,
+    "Temporada disponíveis" : 3,
+    "Linguagem" : "Guarani",
+    "Genero" : ["Comédia"],
+    "IMDb Avaliação" : 8.1,
+})
 db.serie.insertMany([{
     "Série" : "The Marvelous Mrs. Marvel",
     "Temporada disponíveis" : 3,
@@ -67,3 +75,76 @@ db.series.insertMany([{
     "IMDb Avaliação" : 7.5,
     "Classificação" : "16+"
 }])
+
+
+//CONSULTAS
+//Todas as séries com ano de lançamento igual a 2018
+db.series.find({"Ano de lançamento" : 2018})
+
+//Todas as séries com ano de lançamento igual a 2018 ou classificação 18+ (OR)
+db.series.find({$or:[{"Ano de lançamento" : 2018},{"Classificação" : "18+"}]})
+
+//Todas as séries com ano de lançamento igual a 2018 e classificação 18+ (AND)
+db.series.find({$and:[{"Ano de lançamento" : 2018},{"Classificação" : "18+"}]})
+
+//Todas as séries que não tem o ano de lançamento 2018 ou classificação 18+ (NOT OR)
+db.series.find({$nor:[{"Ano de lançamento" : 2018},{"Classificação" : "18+"}]})
+
+//Todas as séries com ano de lançamento 2016 ou 2018 (IN)
+db.series.find({"Ano de lançamento" : {$in : [2016,2018]}})
+
+//Todas as séries com mais de duas temporadas(GREATER THAN)
+db.series.find({"Temporadas Disponíveis" : {$gt : 2}})
+
+//Todas as séries com duas ou mais temporadas(GREATER OR EQUAL THAN)
+db.series.find({"Temporadas Disponíveis" : {$gte : 2}})
+
+//Todas as séries com menos de cinco temporadas(LESS THAN)
+db.series.find({"Temporadas Disponíveis" : {$lt : 5}})
+
+//Todas as séries com cinco ou menos temporadas (LESS OR EQUAL THAN)
+db.series.find({"Temporadas Disponíveis" : {$lte : 5}})
+
+//Filtros
+//Todas as séries com menos de cinco temporadas mas apenas os campos "Série"  e "Linguagem"
+db.series.find({"Temporadas Disponíveis" : {$lte : 5}},{"Série" : 1, "Linguagem" : 1, _id : 0})
+
+//Usando a ordenação por série
+db.series.find({"Temporadas Disponíveis" : {$lte : 5}},
+{"Série" : 1, "Linguagem" : 1, _id : 0}
+).sort({"Série" : 1})
+
+//Buscar todos os registros do banco 
+db.series.find()
+
+//Buscando o campo "Série" e "Ano de lançamento" de todos os registros disponíveis
+db.series.find({},{"Série":1, "Ano de lançamento": 1, "_id":0})
+
+//Buscando registros limitados a cinco registros
+db.series.find().limit(5)
+
+//ATUALIZANDO DADOS
+db.series.find({"Série" : "A nova onda do Kusko"})
+//Utilizando o nome Série para buscar e depois informando dado para atualizar usando $set
+db.series.updateOne({"Série" : "A nova onda do Kusko"},{$set: {"Temporada disponíveis" : 4}})
+
+//Utilizando o comando de atualizar para adiconar um novo campo com a classificação
+db.series.updateOne({"Série" : "A nova onda do Kusko"},{$set: {"Classificação" : "16+"}})
+
+//Buscando duas séries
+db.series.find({"Série" : {$in: ["Four More Shots Please","Flebang"]}})
+
+//Atualizando duas séries ao mesmo tempo
+db.series.updateMany({"Série" : {$in : ["Four More Shots Please", "Flebang"]}},{$set:{"Classificação" : "16+"}})
+
+//Excluindo Dados
+//MUITO CUIDADO COM DELETE, SE NÂO PASSAR O FILTRO TUDO SERÁ EXCLUIDO
+//Como parametro se passa o filtro do que será excluido
+db.series.deleteOne({"Série" : "The Boys"})
+db.series.deleteMany({"Temporadas Disponíveis" : 1})
+
+
+
+
+
+
