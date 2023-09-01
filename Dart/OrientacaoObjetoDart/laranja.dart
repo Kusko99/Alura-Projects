6b580659fdd45a0cfd0bc7c03a767d71d17acc0b
Kusko99 <fernandaofo120@gmail.com>
2023-08-31 22:04:43 -0300

    Start OO in Dart

---
+++ Dart/OrientacaoObjetoDart/laranja.dart
@@ -0,0 +1,14 @@
+void main() {
+  String nome = 'Laranja';
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e cítrica";
+  int diaDesdeColheita = 40;
+  bool isMadura = true;
+
+  if (diaDesdeColheita >= 30) {
+    isMadura = true;
+  } else {
+    isMadura = false;
+  }
+}
