1b80580b2c83b0352dc83f42abccceeedd2a56ca
Kusko99 <fernandaofo120@gmail.com>
2023-09-03 18:59:22 -0300

    More widgtes

--- Flutter/project/lib/main.dart
+++ Flutter/project/lib/main.dart
@@ -14,62 +14,79 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.tealAccent,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.redAccent,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.deepOrange,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.deepPurple,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
+        home: Container(
+            color: Colors.indigoAccent,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.cyan,
-                  height: 50,
-                  width: 50,
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.tealAccent,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.redAccent,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.deepOrange,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.deepPurple,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.cyan,
+                      height: 50,
+                      width: 50,
+                    ),
+                    Container(
+                      color: Colors.pinkAccent,
+                      height: 50,
+                      width: 50,
+                    ),
+                    Container(
+                      color: Colors.amber,
+                      height: 50,
+                      width: 50,
+                    )
+                  ],
                 ),
                 Container(
-                  color: Colors.pinkAccent,
-                  height: 50,
-                  width: 50,
+                  color: Colors.deepOrange,
+                  height: 30,
+                  width: 300,
+                  child: const Text(
+                    'Suco de Laranja',
+                    style: TextStyle(color: Colors.black87, fontSize: 28),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                Container(
-                  color: Colors.amber,
-                  height: 50,
-                  width: 50,
-                )
+                ElevatedButton(
+                    onPressed: () {
+                      print("Você apertou o botão");
+                    },
+                    child: const Text('Aperte o botão!'))
               ],
-            ),
-          ],
-        ));
+            )));
   }
 }
