32531dfd49e67a1fe00386849548d2865734f0a0
Kusko99 <fernandaofo120@gmail.com>
2023-10-31 20:00:39 -0300

    Frist API build using Django rest_framework

---
+++ Django/Django Rest Framework/escola/views.py
@@ -0,0 +1,32 @@
+from rest_framework import viewsets, generics
+from escola.models import Aluno,Curso, Matricula
+from escola.serializer import AlunoSerializer,CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer,ListaAlunosMatriculadosSerializer
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as Matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as Matriculas de um aluno ou aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos e alunas matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
